49bf88a3b875fbd630bd137384a04d5b78cfa472
barberj <barber.justin@gmail.com>
2012-08-01 20:35:10 -0400

    Update license format to be able to register on pypi

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ setup(
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Topic :: Utilities",
-        "License :: MIT License",
+        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.2",
